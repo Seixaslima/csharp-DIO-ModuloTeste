82925c16f45581b82ce2abec9aba1226deacc1d7
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-06-02 22:39:30 -0300

    Implementando classe teste

--- CalculadoraTestes/CalculadoraTests.cs
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -1,10 +1,21 @@
+using Calculadora.Services;
+
 namespace CalculadoraTestes;
 
 public class CalculadoraTests
 {
+    private CalculadoraImp _calc;
+
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+
     [Fact]
     public void DeveSomar5Com10ERetornar15()
     {
-
+        //Arange
+        //Act
+        //Assert
     }
 }
